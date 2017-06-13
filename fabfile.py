916b3af44db2072b3ecff9ca1f9425de37ca36f8
daniel <xiaoaoxtu@gmail.com>
2017-06-13 23:11:25 +0800

    deploy

--- fabfile.py
+++ fabfile.py
@@ -1,3 +1,13 @@
 from fabric.api import run
+from fabric.api import local
 def host_type():
     run('uname -s')
+
+def hello():
+    print("hel")
+
+
+def prepare_deploy():
+    local("git add .")
+    local("git add -p && git commit")
+    local("git push")
